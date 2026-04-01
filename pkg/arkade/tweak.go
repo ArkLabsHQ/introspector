4ea9cce366501a9b364e29a0bc79746ea2375204
Chris Ricketts <chris_ricketts@protonmail.com>
2026-04-01 14:12:06 +0100

    refactor: replace magic value with external secp256k1 pkg constant

--- pkg/arkade/tweak.go
+++ pkg/arkade/tweak.go
@@ -4,6 +4,7 @@ import (
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 )
 
 var (
@@ -40,7 +41,7 @@ func ComputeArkadeScriptPublicKey(pubKey *btcec.PublicKey, scriptHash []byte) *b
 func ComputeArkadeScriptPrivateKey(privKey *btcec.PrivateKey, scriptHash []byte) *btcec.PrivateKey {
 	privKeyScalar := privKey.Key
 	pubKeyBytes := privKey.PubKey().SerializeCompressed()
-	if pubKeyBytes[0] == 0x03 {
+	if pubKeyBytes[0] == secp256k1.PubKeyFormatCompressedOdd {
 		privKeyScalar.Negate()
 	}
 
